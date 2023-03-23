802d025bc92900ce0c26426990606b4bd6475a68
abdurrahman varol <av_0233@outlook.com>
2023-03-23 22:55:46 +0300

    auth layout a arka plan resmi eklendi

--- src/layouts/AuthLayout.js
+++ src/layouts/AuthLayout.js
@@ -3,10 +3,8 @@ import { Outlet } from 'react-router-dom'
 
 function AuthLayout() {
   return (
-    <div>
-      <h1>AuthLayout</h1>
-      <hr></hr>
-      <div style={{ height: "100vh"}} className="d-flex justify-content-center align-items-center">
+    <div className='auth'>      
+      <div style={{ height: "100vh"}} className="d-flex p-5 justify-content-end align-items-center">
         <div className='w-25'>
         <Outlet></Outlet>
         </div>
