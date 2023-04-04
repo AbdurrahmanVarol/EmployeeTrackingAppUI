ee6ac9fde36bb47a047394101f257716887b9eeb
abdurrahman varol <av_0233@outlook.com>
2023-04-04 13:06:20 +0300

    Ticket oluşturma işlemi için düzenleme yapıldı

--- src/pages/CreateTicket.js
+++ src/pages/CreateTicket.js
@@ -1,18 +1,40 @@
 import alertify from 'alertifyjs'
-import React, { useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { Button, Form, FormGroup, Input, Label } from 'reactstrap'
+import DefaultContext from '../contexts/DefaultContext'
 
 function CreateTicket() {
+  const{token} = useContext(DefaultContext)
   const [ticket,setTicket] =useState()
+  const [departments,setDepartments] =useState([])
+  useEffect(()=>{
+    fetch("https://localhost:44333/api/departments")
+    .then((response) => response.json())
+    .then((data) => {
+        setDepartments([...data])
+    });
+  },[])
 
   const changeHandler = (event) =>{
     let name = event.target.name
     let value = event.target.value
     setTicket((prev) => ({ ...prev, [name]: value }));
+    console.log(ticket)
   }
   const submitHandler = (event)=>{
     event.preventDefault()
-    console.log(ticket)
+    fetch("https://localhost:44333/api/jobs", {
+      method: "POST",
+      body: JSON.stringify(ticket),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+         "Authorization" : `Bearer ${token}`
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+          alertify.success("Department Added!")
+      });
     alertify.success("Ticket created")
   }
   return (
@@ -31,13 +53,16 @@ function CreateTicket() {
         <FormGroup>
           <Label for="department">Department</Label>
             <Input
-              id="department"
-              name="department"
+              id="departmentId"
+              name="departmentId"
               type="select"
               onChange={changeHandler}
             >
-              <option defaultValue value="1">Department 1</option>
-              <option value="2">Department 2</option>
+              {
+                departments.map((department,index)=>(
+                  <option key={index} value={department.id}>{department.departmentName}</option>
+                ))
+              }
             </Input>          
         </FormGroup>
         <Button type='submit' onClick={submitHandler}>Create</Button>
